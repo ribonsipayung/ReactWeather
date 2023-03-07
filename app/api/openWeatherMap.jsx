f3a07c8b145975f2defa55d6d9803e5df8925e0c
ribonsipayung <ribonsipayung412@gmail.com>
2023-03-07 10:46:11 +0700

    Initial commit

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,22 @@
+var axios = require('axios');
+
+const OPEN_WEATHER_MAP_URL = 'https://api.openweathermap.org/data/2.5/weather?id=524901&lang=id&appid=8e4207c78deee2801e73fc52d654a82b&units=imperial';
+
+module.exports={
+    getTemp: function(location){
+        var encodedLocation=encodeURIComponent(location);
+        var requestUrl=`${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+
+        return axios.get(requestUrl).then(function (res){
+            debugger;
+            if(res.data.cod && res.data.message){
+                throw new Error(res.data.message);
+            }else{
+                return res.data.main.temp;
+            }
+        }, function(res){
+            throw new Error(res.data.message);
+        });
+        
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,97 @@
+const path = require('path');
+module.exports={
+    mode: 'development',
+    entry: './app/app.jsx',
+    output: {
+        path: __dirname,
+        filename: './public/bundle.js'
+    },
+    // resolve: {
+    //     modules: [__dirname, 'public/components'],
+    //     alias: {
+    //       Greeter: 'Greeter.jsx',
+    //       GreeterMessage: 'GreeterMessage.jsx',
+    //       GreeterForm: 'GreeterForm.jsx'
+    //     },
+
+    resolve: {
+        alias: {
+            Main: path.resolve(__dirname, 'app/components/Main.jsx'),
+            Nav: path.resolve(__dirname, 'app/components/Nav.jsx'),
+            Weather: path.resolve(__dirname, 'app/components/Weather.jsx'),
+            WeatherForm: path.resolve(__dirname, 'app/components/WeatherForm.jsx'),
+            WeatherMessage: path.resolve(__dirname, 'app/components/WeatherMessage.jsx'),
+            About: path.resolve(__dirname, 'app/components/About.jsx'),
+            Examples: path.resolve(__dirname, 'app/components/Examples.jsx'),
+            openWeatherMap: path.resolve(__dirname, 'app/api/openWeatherMap.jsx'),
+        },
+        modules: [
+          path.resolve(__dirname, 'node_modules')
+        ],
+        extensions: ['', '.js', '.jsx']
+      },
+      
+    // resolve:{
+    //     root: __dirname,
+    //     alias:{
+    //         Greeter: 'public/components/Greeter.jsx',
+    //         GreeterMessage: 'public/components/GreeterMessage.jsx',
+    //         GreeterForm: 'public/components/GreeterForm.jsx'
+    //     },
+    //     extensions: ['', '.js', '.jsx']
+    // },
+    module:{
+        rules:[
+            {
+                loader: 'babel-loader',
+                options:{
+                    presets:['@babel/preset-react', '@babel/preset-env']
+                },
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/
+            }
+        ]
+    },
+    devtool: 'eval-source-map'
+    // devtool: 'cheap-module-eval-source-map'
+};
+
+
+
+
+
+
+
+//     module:{
+//         loaders:[
+//             {loader: 'babel-loader',
+//             query:{
+//                 presets: ['@babel/preset-react', '@babel/preset-env']
+//             },
+//             test: /\.jsx?$/,
+//             exclude: /(node_modules|bower_components)/
+//         }
+//     ]
+// }
+// };
+    // resolve:{
+    //     root: __dirname,
+    //     // alias:{
+    //     //     Greeter: 'public/components/Greeter.jsx',
+    //     //     GreeterMessage: 'public/components/GreeterMessage.jsx',
+    //     //     GreeterForm: 'public/components/GreeterForm.jsx'
+    //     // },
+    //     extensions:['', '.js', '.jsx']
+    // }
+    // module:{
+    //     loaders:[
+    //         {
+    //             loader: 'babel-loader',
+    //             query:{
+    //                 presets:['react', 'es2015']
+    //             },
+    //             test: /\.jsx?$/,
+    //             exclude: /(node_modules|bower_components)/
+    //         }
+    //     ]
+    // }
