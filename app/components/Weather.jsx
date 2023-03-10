2e926c61330d7913a6841e96ed5acf9ba3670048
ribonsipayung <ribonsipayung412@gmail.com>
2023-03-10 16:29:41 +0700

    aaaa

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -70,7 +70,7 @@ var Weather = React.createClass({
 
     return (
       <div>
-        <h1 className="text-center page-title">Get Weather</h1>
+        <h1 className="text-center page-title">Get Weather 1</h1>
         <WeatherForm onSearch={this.handleSearch}/>
         {renderMessage()}
         {renderError()}
